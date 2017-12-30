77efdccd62f5b8d07879ee69a0e4a41a006e7686
Kris Bellemans <kris.bellemans@technomatic.be>
2017-12-30 19:21:02 +0000

    Revert "Add servo control test code"
    This reverts commit 7653099bab0e7989d0af84be7f4fa4ed1907537d.

--- main.c
+++ main.c
@@ -4,7 +4,6 @@
 #include "math.h"
 #include "stdio.h"
 #include "stm32f4xx_usart.h"
-#include "stm32f4xx_tim.h"
 
 // Macro to use CCM (Core Coupled Memory) in STM32F4
 #define CCM_RAM __attribute__((section(".ccmram")))
@@ -15,12 +14,6 @@ StackType_t fpuTaskStack[FPU_TASK_STACK_SIZE] CCM_RAM;  // Put task stack in CCM
 StaticTask_t fpuTaskBuffer CCM_RAM;  // Put TCB in CCM
 
 void init_USART3(void);
-void init_timer(void);
-void PositionServoPulse(uint32_t pulse);
-
-
-__IO uint16_t ServoMinAngle = 20;		// 0
-__IO uint16_t ServoMaxAngle = 2500;		// 180
 
 void test_FPU_test(void* p);
 
@@ -28,7 +21,6 @@ int main(void) {
   SystemInit();
   NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4);
   init_USART3();
-  init_timer();
 
   // Create a task
   // Stack and TCB are placed in CCM of STM32F4
@@ -113,132 +105,18 @@ void vApplicationGetTimerTaskMemory(StaticTask_t **ppxTimerTaskTCBBuffer, StackT
   *pulTimerTaskStackSize = configTIMER_TASK_STACK_DEPTH;
 }
 
-void SetServoPosition(uint32_t angle){
-	uint32_t range = ServoMaxAngle - ServoMinAngle;
-	uint32_t unit = range / 180;
-
-	uint32_t step = unit * angle;
-
-	if(step == 0)
-		step = ServoMinAngle;
-
-	if(step > ServoMaxAngle)
-		step = ServoMaxAngle;
-
-	PositionServoPulse(step);
-}
-
-void PositionServoPulse(uint32_t pulse){
-	  TIM_OCInitTypeDef  TIM_OCInitStructure;
-	/* Configure timer for PWM - channel 1*/
-	  TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
-	  TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
-	  TIM_OCInitStructure.TIM_Pulse = pulse;
-	  TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
-
-	  //notice the number 1 in TIM_OC1Init
-	  TIM_OC1Init(TIM4, &TIM_OCInitStructure);
-	  TIM_OC1PreloadConfig(TIM4, TIM_OCPreload_Enable);
-}
-
-/**
-  * @brief  Delay Function.
-  * @param  nCount:specifies the Delay time length.
-  * @retval None
-  */
-void Delay(__IO uint32_t nCount)
-{
-	while(nCount--)
-	{
-	}
-}
-
 void test_FPU_test(void* p) {
-	float ff = 1.0f;
-	printf("Start FPU test task.\n");
-	for (;;) {
-		float s = sinf(ff);
-		ff += 1.0f;
-		//ff += s;
-		printf("sin: %f\r\n", s);
-		SetServoPosition(90);
-		//SetServoPosition(180);
-		//SetServoPosition(0);
+  float ff = 1.0f;
+  printf("Start FPU test task.\n");
+  for (;;) {
+    float s = sinf(ff);
+    ff += s;
+    // TODO some other test
 
-		/*
-		SetServoPosition(0);
-		Delay(50000000);
+    vTaskDelay(1000);
+  }
 
-		SetServoPosition(90);
-		Delay(50000000);
-
-		SetServoPosition(180);
-		Delay(50000000);
-		*/
-		// TODO some other test
-
-		vTaskDelay(100);
-	}
-
-	vTaskDelete(NULL);
-}
-
-void SetupPins(){
-	/* Enable TIM4 Clock */
-	  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);
-
-
-	  //Enabled GPIOB we are going to use PB6 which is linked to TIM4_CH1 according to the
-	  //documentation
-	  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
-
-	  GPIO_InitTypeDef GPIO_InitStructure;
-	  //initalise pin 6 B - relating to timer 4 channel 1
-	  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6 ;
-	  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
-	  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
-	  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-	  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL ;
-	  GPIO_Init(GPIOB, &GPIO_InitStructure);
-
-	  //link the pins up to the timer - AF stands for alternate function
-	  GPIO_PinAFConfig(GPIOB, GPIO_PinSource6, GPIO_AF_TIM4);
-}
-
-void init_timer(void)
-{
-	uint32_t prescalar = (uint16_t) ( ( (SystemCoreClock / 2) / 1000000) - 1);
-	uint32_t period = 20000;
-	SetupPins();
-
-	/*Structures used in the configuration*/
-	TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
-	TIM_OCInitTypeDef  TIM_OCInitStructure;
-
-	/* Setup timer defaults */
-	TIM_TimeBaseStructure.TIM_Period = period;
-	TIM_TimeBaseStructure.TIM_Prescaler = prescalar;
-	TIM_TimeBaseStructure.TIM_ClockDivision = 0;
-	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
-	TIM_TimeBaseStructure.TIM_RepetitionCounter = 0;
-
-	TIM_TimeBaseInit(TIM4, &TIM_TimeBaseStructure);
-
-	/* Configure timer for PWM - channel 1*/
-	TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
-	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
-	TIM_OCInitStructure.TIM_Pulse = 0;
-	TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
-
-	//notice the number 1 in TIM_OC1Init
-	TIM_OC1Init(TIM4, &TIM_OCInitStructure);
-	TIM_OC1PreloadConfig(TIM4, TIM_OCPreload_Enable);
-
-
-	TIM_ARRPreloadConfig(TIM4, ENABLE);
-
-	/* Start timer*/
-	TIM_Cmd(TIM4, ENABLE);
+  vTaskDelete(NULL);
 }
 
 /*
